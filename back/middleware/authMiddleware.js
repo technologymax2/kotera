784b25a0eff80149ce0eb5d61fee7165df53668a
technologymax2 <technologymax2@gmail.com>
2026-09-13 21:33:54 -0700

    Update authMiddleware.js

--- back/middleware/authMiddleware.js
+++ back/middleware/authMiddleware.js
@@ -33,7 +33,7 @@ const protect = async (req, res, next) => {
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.id).select("-password");
+   const user = await FaceUser.findById(decoded.id).select("-password");
 
     if (!user) {
       if (allowTempLogin) {
